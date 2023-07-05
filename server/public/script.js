a426ca5ccb50079677f1a55d1e79b6526bb90d79
Bruno Novais <brn.kof@gmail.com>
2023-07-05 00:23:27 -0300

    fix formulario para edição de botões

--- server/public/script.js
+++ server/public/script.js
@@ -1,6 +1,6 @@
 const buttonWrapper = document.querySelector(".button-wrapper");
 const checkEdit = document.querySelector('input[type="checkbox"]');
-
+//const submitEdit = document.querySelector(".submit-edit");
 let buttonsData;
 let numButtons;
 
@@ -29,65 +29,59 @@ function createButtons() {
     .map((_, index) => {
       const buttonId = index;
       return `
-      <div class='button' id='button-${buttonId}'>
-        <span class='button-label'>vazio ${buttonId}</span>
-      </div>
-    `;
+        <div class='button' id='button-${buttonId}'>
+          <span class='button-label'>vazio ${buttonId}</span>
+        </div>
+      `;
     })
     .join("");
 
   loadButtonsData().then(() => {
-    buttonsData.slice(0, numButtons).forEach(botao => {
-      if (botao.id !== undefined && botao.id <= numButtons) {
-        const buttonLabel = document.querySelector(`#button-${botao.id} .button-label`);
-        if (botao.labelButton) {
-          buttonLabel.innerText = botao.labelButton;
-        } else {
-          buttonLabel.innerText = "";
-        }
-        const sound = new Audio(`http://localhost:3000${botao.path}`);
-        //const sound = new Audio(`http://localhost:5500/src/sounds/${botao.id}.mp3`);
-        const buttonPath = document.querySelector(`#button-${botao.id}`);
-
-        buttonPath.onclick = function () {
-          let check = checkEdit;
-          if (check.checked) {
-            const editaJson = () => {
-              // alert("test");
-              console.log(buttonsData);
-            };
-            editaJson();
-          } else {
-           // sound.addEventListener('loadeddata', function(){
-           //   console.log('audio carregado');
-              
-           // })
-            sound.play();
-            
-          }
-        };
-      }
-    });
+    updateButtonsData();
   });
 }
 
 function loadButtonsData() {
-  return fetch('buttons.json')
+  return fetch('src/buttons.json')
     .then(response => response.json())
     .then(data => {
       buttonsData = data;
     });
 }
 
-createButtons();
+function updateButtonsData() {
+  buttonsData.slice(0, numButtons).forEach(botao => {
+    if (botao.id !== undefined && botao.id <= numButtons) {
+      const buttonLabel = document.querySelector(`#button-${botao.id} .button-label`);
+      if (botao.labelButton) {
+        buttonLabel.innerText = botao.labelButton;
+      } else {
+        buttonLabel.innerText = "";
+      }
+      const sound = new Audio(`http://localhost:3000${botao.path}`);
+      const buttonPath = document.querySelector(`#button-${botao.id}`);
 
-window.addEventListener("resize", () => {
+      buttonPath.onclick = function () {
+        let check = checkEdit;
+        if (check.checked) {
+          openPopup();
+        } else {
+          sound.play();
+        }
+      };
+    }
+  });
+}
+
+function editaJson() {
+  console.log(buttonsData);//test
+}
+
+function handleWindowResize() {
   createButtons();
-  // Utiliza a variável buttonsData para alterar os labels dos botões após a geração
   if (buttonsData) {
     buttonsData.slice(0, numButtons).forEach(botao => {
       if (botao.id !== undefined && botao.id < numButtons) {
-        let v = botao.id+1;
         const buttonLabel = document.querySelector(`#button-${botao.id} .button-label`);
         if (botao.labelButton) {
           buttonLabel.innerText = botao.labelButton;
@@ -97,4 +91,84 @@ window.addEventListener("resize", () => {
       }
     });
   }
-});
+}
+
+
+function openPopup() {
+  const popupHTML = `
+    <div class="popup-container">
+      <form class="popup-form">
+        <label for="field1">Field 1:</label>
+        <input type="text" id="field1" required>
+        <label for="field2">Field 2:</label>
+        <input type="text" id="field2" required>
+        <label for="field3">Field 3:</label>
+        <input type="text" id="field3" required>
+        <button type="submit" class="popup-submit">Submit</button>
+        <button type="button" class="popup-close">Fechar</button>
+      </form>
+    </div>
+  `;
+
+  // Create the popup container element
+  const popupContainer = document.createElement("div");
+  popupContainer.classList.add("popup-container");
+  popupContainer.innerHTML = popupHTML;
+
+  // Disable main window buttons
+  const mainButtons = document.querySelectorAll(".button");
+  mainButtons.forEach(button => {
+    button.style.pointerEvents = "none";
+  });
+
+  // Append popup to the body
+  document.body.appendChild(popupContainer);
+
+  // Handle form submission
+  const popupForm = document.querySelector(".popup-form");
+  popupForm.addEventListener("submit", handleSubmit);
+
+  // Close the popup and re-enable main window buttons
+  function closePopup() {
+    popupContainer.remove();
+    mainButtons.forEach(button => {
+      button.style.pointerEvents = "auto";
+    });
+  }
+
+  // Close the popup on button click
+  const closeButton = document.querySelector(".popup-close");
+  closeButton.addEventListener("click", closePopup);
+
+  // Close the popup on Escape key press
+  document.addEventListener("keydown", function(event) {
+    if (event.key === "Escape") {
+      closePopup();
+    }
+  });
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    // Retrieve field values
+    const field1Value = document.querySelector("#field1").value;
+    const field2Value = document.querySelector("#field2").value;
+    const field3Value = document.querySelector("#field3").value;
+
+    // Perform desired actions with the field values
+    // ...
+
+    // Close the popup
+    closePopup();
+  }
+}
+
+
+
+
+
+
+
+createButtons();
+
+window.addEventListener("resize", handleWindowResize);
--- server/server.js
+++ server/server.js
@@ -7,6 +7,15 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use('/', routes);
 
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`Server running on port ${port}`);
+
+  const open = await import('open').then(m => m.default || m);
+
+  const url = 'http://localhost:3000';
+
+  // Abrir a URL no navegador padrão
+  open(url)
+    .then(() => console.log('Navegador aberto com sucesso!'))
+    .catch(err => console.error('Erro ao abrir o navegador:', err));
 });
